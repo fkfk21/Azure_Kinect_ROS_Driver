196d3ae5b24fcd20aa094c54b137a20bb0ee6a08
Ryuma Sugihara <sugihara.r.ad@m.titech.ac.jp>
2024-04-12 18:22:36 +0900

    shared_ptr version

--- src/k4a_ros_bridge_node.cpp
+++ src/k4a_ros_bridge_node.cpp
@@ -19,44 +19,44 @@ int main(int argc, char** argv)
   rclcpp::init(argc, argv);
 
   // Create Node for handling info and error messages
-  rclcpp::Node::SharedPtr node = rclcpp::Node::make_shared("k4a_bridge");
+  // rclcpp::Node::SharedPtr node = rclcpp::Node::make_shared("k4a_bridge");
 
 
   // Setup the K4A device
-  std::shared_ptr<K4AROSDevice> device(new K4AROSDevice);
+  rclcpp::Node::SharedPtr device = std::make_shared<K4AROSDevice>();
 
-  k4a_result_t result = device->startCameras();
+  // k4a_result_t result = device->startCameras();
+ 
+  // if (result != K4A_RESULT_SUCCEEDED)
+  // {
+  //   RCLCPP_ERROR_STREAM(node->get_logger(),"Failed to start cameras");
+  //   return -1;
+  // }
+ 
+  // result = device->startImu();
+  // if (result != K4A_RESULT_SUCCEEDED)
+  // {
+  //   RCLCPP_ERROR_STREAM(node->get_logger(),"Failed to start IMU");
+  //   return -2;
+  // }
+ 
+  // RCLCPP_INFO(node->get_logger(),"K4A Started");
+ 
+  // if (result == K4A_RESULT_SUCCEEDED)
+  // {
+    rclcpp::spin(device);
 
-  if (result != K4A_RESULT_SUCCEEDED)
-  {
-    RCLCPP_ERROR_STREAM(node->get_logger(),"Failed to start cameras");
-    return -1;
-  }
+    // RCLCPP_INFO(node->get_logger(),"ROS Exit Started");
+  // }
 
-  result = device->startImu();
-  if (result != K4A_RESULT_SUCCEEDED)
-  {
-    RCLCPP_ERROR_STREAM(node->get_logger(),"Failed to start IMU");
-    return -2;
-  }
+  // device.reset();
 
-  RCLCPP_INFO(node->get_logger(),"K4A Started");
-
-  if (result == K4A_RESULT_SUCCEEDED)
-  {
-    rclcpp::spin(node);
-
-    RCLCPP_INFO(node->get_logger(),"ROS Exit Started");
-  }
-
-  device.reset();
-
-  RCLCPP_INFO(node->get_logger(),"ROS Exit");
+  // RCLCPP_INFO(node->get_logger(),"ROS Exit");
 
   rclcpp::shutdown();
 
-  RCLCPP_INFO(node->get_logger(),"ROS Shutdown complete");
+  // RCLCPP_INFO(node->get_logger(),"ROS Shutdown complete");
 
-  RCLCPP_INFO(node->get_logger(),"Finished ros bridge main");
+  // RCLCPP_INFO(node->get_logger(),"Finished ros bridge main");
   return 0;
 }
